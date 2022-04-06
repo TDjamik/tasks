9f7857d808c6caddf48d741ecef88fed9d1011ea
TDjamik <abuabdulaziz3031@gmail.com>
2022-04-06 04:06:30 +0300

    second commit

--- main.dart
+++ main.dart
@@ -31,3 +31,22 @@ void main() {
     }
   } while (con);
 }
+/*
+task 2
+void main() {
+  int n = 1;
+  int count = 0;
+  for (int i = 1; i < n; i++) {
+    if (isPrime(i)) count++;
+  }
+  print(count);
+}
+
+bool isPrime(int a) {
+  if (a <= 1) return false;
+  for (int i = 2; i * i <= a; i++) {
+    if (a % i == 0) return false;
+  }
+  return true;
+}
+ */
